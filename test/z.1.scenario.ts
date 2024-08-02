69362d3717de468c3ad7e465d12eb0869f03214d
Zena-park <zena@tokamak.network>
2024-08-02 14:28:31 +0900

    test: the 1st scenario #4

--- test/z.1.scenario.ts
+++ test/z.1.scenario.ts
@@ -9,11 +9,24 @@ import hre from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 import type { Signers } from "./types";
-
+import { padLeft } from 'web3-utils'
+import { marshalString, unmarshalString } from './shared/marshal';
 // import {deployGovernanceContractsFixture } from "./Goverance/Governance.fixture";
 import {tonStakingV2ContractsFixture } from "./TonStakingV2.fixture";
 
-describe("TokamakGovernor Test", async function () {
+import { tonStakingV2Config } from "../tostakingv2.config"
+import { config } from "../deploy.tokamak.tally.config"
+
+/**
+ *
+scenario 1.
+(1) A Voting Rights Mint (200 tokens), B Voting Rights Mint (800 tokens)
+(2) Vote: A for, B against
+(3) Additional A voting rights (800 tokens) created during the voting period
+(4) Voting result after the voting period ends?
+ */
+
+describe("TokamakGovernor : the 1st scenario", async function () {
     before(async function () {
         this.signers = {} as Signers;
 
@@ -23,9 +36,8 @@ describe("TokamakGovernor Test", async function () {
         this.signers.admin = signers[1];
         this.signers.notAuthorized = signers[2];
 
-        console.log("deployer ", this.deployer.address)
-        console.log("admin ", this.signers.admin.address)
-        console.log("notAuthorized ", this.signers.notAuthorized.address)
+        this.aUser  = signers[3];
+        this.bUser  = signers[4];
 
         this.loadFixture = loadFixture;
     });
@@ -38,6 +50,8 @@ describe("TokamakGovernor Test", async function () {
             } = await this.loadFixture(tonStakingV2ContractsFixture);
 
         const daoAddress = await dao.getAddress()
+
+        this.proposalId ;
 
         this.governor = governor;
         this.tokamaktoken = tokamaktoken;
@@ -53,46 +67,26 @@ describe("TokamakGovernor Test", async function () {
         this.tos = tos;
         this.stakingHolder = stakingHolder;
 
-        console.log("daoAddress", daoAddress)
-
         await network.provider.send("hardhat_impersonateAccount", [ daoAddress, ]);
         await network.provider.send("hardhat_setBalance", [ daoAddress,  "0x10000000000000000000000000", ]);
 
         this.daoSigner = await ethers.getSigner(daoAddress);
+
+        //===============================
+        // agenda : mint 1000 ton to admin
+        const calldata = this.ton.interface.encodeFunctionData("mint", [this.signers.admin.address, 1000n]);
+        //===============================
+
+        this.proposalInfo = {
+            targets: [await this.ton.getAddress()],
+            value: [0n],
+            calldata:  [calldata],
+            description: "Proposal to mint 1000 tokens for admin"
+        }
     });
 
-    it("should receive answer from CLOCK_MODE", async function () {
-        const { governor, _, } = this;
+    before(async function () {
 
-        const clock_mode = await governor.CLOCK_MODE();
-
-        expect(clock_mode).to.be.equal("mode=blocknumber&from=default");
-    });
-
-    it("clock should return the current block number", async function () {
-        const { governor, _, } = this;
-
-        const clock = await governor.clock();
-        const pBlock = await ethers.provider.getBlock("latest");
-
-        expect(clock).to.be.equal(pBlock?.number);
-    });
-
-    it("If SWTON is insufficient, mint cannot be performed.", async function () {
-        const { tokamaktoken, governor, signers, timelock } = this;
-
-        const amountToMint = 10000n;
-        let stakedAmount = await this.seigManager.stakeOf(signers.admin.address)
-        let mintableAmount = await tokamaktoken.mintableAmount(signers.admin.address)
-        expect(mintableAmount).to.be.eq(0n)
-
-        await expect(
-            tokamaktoken.connect(signers.admin).mint(amountToMint)
-        ).to.be.rejectedWith("InsufficientStakedAmount()")
-
-    });
-
-    it("Grant TON minter permission to timelock before creating agenda", async function () {
         const { tokamaktoken, governor, signers, timelock } = this;
         expect(await this.ton.isMinter(this.daoSigner.address)).to.be.true;
         const timelock_address = await timelock.getAddress();
@@ -106,53 +100,69 @@ describe("TokamakGovernor Test", async function () {
             data: event?.data ?? "",
         });
         expect(logDescription?.args["account"]).to.be.eq(timelock_address)
+
     })
 
-    it("should work the full proposal lifecycle up to executed", async function () {
+    it("A stake TON, B stake TON", async function () {
+
+        await (await this.ton.connect(this.daoSigner).mint(this.aUser.address, 1100000000000000000000n)).wait()
+        await (await this.ton.connect(this.daoSigner).mint(this.bUser.address, 1100000000000000000000n)).wait()
+
+        const depositManager_address = await this.depositManager.getAddress()
+        const depositAmount1000 = 1100000000000000000000n
+
+        const data = marshalString(
+            [depositManager_address, tonStakingV2Config.LevelCandidate]
+              .map(unmarshalString)
+              .map(str => padLeft(str, 64))
+              .join(''),
+        );
+
+        const receipt1 = await (await this.ton.connect(this.aUser).approveAndCall(
+            tonStakingV2Config.WTON,
+            depositAmount1000,
+            data
+        )).wait()
+
+        expect(receipt1).to.emit(this.depositManager, "Deposited");
+
+        const receipt2 = await (await this.ton.connect(this.bUser).approveAndCall(
+            tonStakingV2Config.WTON,
+            depositAmount1000,
+            data
+        )).wait()
+
+        expect(receipt2).to.emit(this.depositManager, "Deposited");
+
+
+    });
+
+    it("(1) A Voting Rights Mint (200 tokens), B Voting Rights Mint (800 tokens)", async function () {
+
         const { tokamaktoken, governor, signers, timelock } = this;
-
-        const balancePrev = await this.ton.balanceOf(signers.admin.address);
-
-        // mint (by minter) -> _update -> _transferVotingUnits
-        let holder_address = await this.stakingHolder.getAddress()
-        let stakedAmount = await this.seigManager.stakeOf(holder_address)
-        let mintableAmount = await tokamaktoken.mintableAmount(holder_address)
-        expect(mintableAmount).to.be.eq(stakedAmount / 1000000000n)
-        expect(mintableAmount).to.be.gt(0)
-
-        await tokamaktoken.connect(this.stakingHolder).mint(mintableAmount);
-
-        const balanceOne = await tokamaktoken.balanceOf(this.stakingHolder.address);
-        expect(balanceOne).to.be.equal(mintableAmount);
-
-        // getVotes
-        let clock0  = await governor.clock();
+        const amount_200 = BigInt("2"+"0".repeat(20))
+        const amount_800 = BigInt("8"+"0".repeat(20))
+        await (await tokamaktoken.connect(this.aUser).mint(amount_200)).wait()
+        await (await tokamaktoken.connect(this.bUser).mint(amount_800)).wait()
 
         // delegate - Delegates votes from the sender to `delegatee`.
-        await tokamaktoken.connect(this.stakingHolder).delegate(this.stakingHolder.address);
+        await tokamaktoken.connect(this.aUser).delegate(this.aUser.address);
+        await tokamaktoken.connect(this.bUser).delegate(this.bUser.address);
 
-        //===============================
-        // agenda : mint 1000 ton to admin
-        const calldata = this.ton.interface.encodeFunctionData("mint", [signers.admin.address, 1000n]);
-        //===============================
+    });
 
-        const proposalInfo = {
-            targets: [await this.ton.getAddress()],
-            value: [0n],
-            calldata:  [calldata],
-            description: "Proposal to mint 1000 tokens for admin"
-        }
+    it("(2) Vote: A for, B against", async function () {
+        const { tokamaktoken, governor, signers, timelock } = this;
 
         // Propose
-        const proposalTx = await governor.propose(
-            proposalInfo.targets, // targets
-            proposalInfo.value, // value
-            proposalInfo.calldata,
-            proposalInfo.description// description
+        const proposalTx = await governor.connect(this.aUser).propose(
+            this.proposalInfo.targets, // targets
+            this.proposalInfo.value, // value
+            this.proposalInfo.calldata,
+            this.proposalInfo.description// description
         );
 
         expect(proposalTx).to.emit(governor, "ProposalCreated");
-
         // Wait for the transaction to be mined
         const receipt = await proposalTx.wait(1);
         const eventLogs: EventLog[] = (receipt?.logs ?? []).filter((log): log is EventLog => true);
@@ -166,91 +176,94 @@ describe("TokamakGovernor Test", async function () {
         });
 
         // Get the proposalId from the event arguments
-        const proposalId = logDescription?.args["proposalId"]
-        expect(logDescription?.args["description"]).to.be.eq(proposalInfo.description)
+        this.proposalId = logDescription?.args["proposalId"]
+        expect(logDescription?.args["description"]).to.be.eq(this.proposalInfo.description)
 
-        const support = 1 // options 0 = Against, 1 = For, 2 = Abstain
-        // try to cast before voting delay and fails
-        await expect( governor.connect(this.stakingHolder).castVote(proposalId, support)).to.be.reverted;
-
-        const numberOfBlocks = Number(await governor.votingDelay()) + 100;
+        //
+        const numberOfBlocks = Number(await governor.votingDelay()) + 1;
         await mine(numberOfBlocks);
-        let clock1  = await governor.clock();
 
-        // Vote
-        await expect( governor.connect(this.stakingHolder).castVote(proposalId, support)).to.emit(governor, "VoteCast");
+        let clock0  = await governor.clock();
+
+        // options 0 = Against, 1 = For, 2 = Abstain
+        // Vote: A for,
+        await expect( governor.connect(this.aUser).castVote(this.proposalId, 1)).to.emit(governor, "VoteCast");
+
+        // Vote: B against
+        await expect( governor.connect(this.bUser).castVote(this.proposalId, 2)).to.emit(governor, "VoteCast");
 
         // getVotes
-        expect(await governor.getVotes(this.stakingHolder.address, clock0)).to.be.eq(0)
-        expect(await governor.getVotes(this.stakingHolder.address, clock1)).to.be.eq(mintableAmount)
+        let getVotes_a = await governor.getVotes(this.aUser.address, clock0)
+        let getVotes_b = await governor.getVotes(this.bUser.address, clock0)
 
-        //try to queue before is executable and fails
+        expect(getVotes_a).to.be.eq(BigInt("2"+"0".repeat(20)))
+        expect(getVotes_b).to.be.eq(BigInt("8"+"0".repeat(20)))
+    });
 
-        // Queue proposal
-        // await  expect( governor.queue(proposalId)).to.be.reverted;
-       await  expect( governor.queue(
-            proposalInfo.targets,
-            proposalInfo.value,
-            proposalInfo.calldata,
-            ethers.keccak256(ethers.toUtf8Bytes(proposalInfo.description)) // description
-       )).to.be.reverted;
+    it("(3) Additional A voting rights (800 tokens) created during the voting period", async function () {
 
-        // Wait for voting period to end
-        // await ethers.provider.send("evm_increaseTime", [86400]); // Increase time by 1 day
-        // await ethers.provider.send("evm_mine"); // Mine a new block
-        await mine(Number(await governor.votingPeriod()) + 100);
+        const { tokamaktoken, governor, signers, timelock } = this;
+        const amount_800 = BigInt("8"+"0".repeat(20))
+        await (await tokamaktoken.connect(this.aUser).mint(amount_800)).wait()
+
+        let clock0  = await governor.clock();
+        await mine(2);
+
+        // getVotes
+        let getVotes_a = await governor.getVotes(this.aUser.address, clock0)
+        let getVotes_b = await governor.getVotes(this.bUser.address, clock0)
+
+        expect(getVotes_a).to.be.eq(BigInt("10"+"0".repeat(20)))
+        expect(getVotes_b).to.be.eq(BigInt("8"+"0".repeat(20)))
+    });
+
+    it("(4) Voting result after the voting period ends? -> It will be executed ", async function () {
+
+        const { tokamaktoken, governor, signers, timelock } = this;
+        await mine(Number(await governor.votingPeriod()) + 1);
 
         // expect proposal state to be succeeded
-        let proposalState = await governor.state(proposalId);
+        let proposalState = await governor.state(this.proposalId);
         expect(proposalState).to.be.equal(4);
 
         // Queue proposal
         await expect( governor.queue(
-            proposalInfo.targets,
-            proposalInfo.value,
-            proposalInfo.calldata,
-            ethers.keccak256(ethers.toUtf8Bytes(proposalInfo.description))
+        this.proposalInfo.targets,
+        this.proposalInfo.value,
+        this.proposalInfo.calldata,
+            ethers.keccak256(ethers.toUtf8Bytes(this.proposalInfo.description))
         )).to.emit(governor, "ProposalQueued");
 
         // expect proposal state to be queued
-        proposalState = await governor.state(proposalId);
+        proposalState = await governor.state(this.proposalId);
         expect(proposalState).to.be.equal(5);
 
-        // Execute proposal
-        await expect( governor.connect(signers.notAuthorized).execute(
-            proposalInfo.targets,
-            proposalInfo.value,
-            proposalInfo.calldata,
-            ethers.keccak256(ethers.toUtf8Bytes(proposalInfo.description))
-        )).to.be.reverted;
-
         // Simulate time delay required before execution
-        // Replace 'executionDelay' with your contract's specific delay
-        await mine( 86400 +1);
+        await mine(config.timelock.minDelay +1);
+
 
         // Execute proposal
         await expect( governor.connect(signers.notAuthorized).execute(
-            proposalInfo.targets,
-            proposalInfo.value,
-            proposalInfo.calldata,
-            ethers.keccak256(ethers.toUtf8Bytes(proposalInfo.description))
+            this.proposalInfo.targets,
+            this.proposalInfo.value,
+            this.proposalInfo.calldata,
+            ethers.keccak256(ethers.toUtf8Bytes(this.proposalInfo.description))
         )).to.emit(governor, "ProposalExecuted");
 
         // expect proposal state to be executed
-        proposalState = await governor.state(proposalId);
+        proposalState = await governor.state(this.proposalId);
         expect(proposalState).to.be.equal(7);
 
-        // Check if admin's balance has increased
-        const balance = await this.ton.balanceOf(signers.admin.address);
-        expect(balance).to.be.equal(balancePrev + 1000n);
+        // enum ProposalState {
+        //     Pending,
+        //     Active,
+        //     Canceled,
+        //     Defeated,
+        //     Succeeded,
+        //     Queued,
+        //     Expired,
+        //     Executed
+        // }
     });
-
-    it("Scenario 1", async function () {
-        console.log("(1) A Voting Rights Mint (200 tokens), B Voting Rights (800 tokens)")
-        console.log("(2) Votes : A for, B against")
-        console.log("(3) Additional A voting rights (800 tokens) created during the voting period ")
-        console.log("(4) Voting result after the voting period ends : ")
-
-    })
 
 });
